c11eba353c56cb0cb3403aa84e5771b5c75159b0
Alex <boza3511@stthomas.edu>
2026-02-18 14:27:47 -0600

    commit

--- pointers.cpp
+++ pointers.cpp
@@ -4,8 +4,8 @@
 
 typedef struct Student {
     int id;
-    char *f_name;
-    char *l_name;
+    char *f_name[128];
+    char *l_name[128];
     int n_assignments;
     double *grades;
 } Student;
@@ -18,11 +18,29 @@ int main(int argc, char **argv)
 {
     Student student;
     double average;
+    char first[128];
+    char last[128]; 
 
     // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999);
+    std::cin.ignore();
+
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> first;
+    student.f_name[0] = first;
+
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> last;
+    student.l_name[0] = last;
+
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217728);
 
     // Call `CalculateStudentAverage(???, ???)`
     // Output `average`
+    // average = calculateStudentAverage(student.grades, average);
+
+    std::cout << student.f_name[0] << " " << student.l_name[0] << " [" << student.id << "]" << std::endl;
+    // std::cout << "  Average grade: " << average/student.n_assignments;
 
     return 0;
 }
@@ -34,7 +52,19 @@ int main(int argc, char **argv)
 */
 int promptInt(std::string message, int min, int max)
 {
-    // Code to prompt user for an int
+    int value = min - 1;
+    while (value < min || value > max) {
+        std::cout << message;
+        std::cin >> value;
+
+        if (value < min || value > max) {
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+            // std::cin.ignore();
+        }
+        std::cin.ignore();
+    }
+
+    return value;
 }
 
 /*
@@ -45,6 +75,19 @@ int promptInt(std::string message, int min, int max)
 double promptDouble(std::string message, double min, double max)
 {
     // Code to prompt user for a double
+    double val = min - 1;
+    while (val < min || val > max) {
+        std::cout << message;
+        std::cin >> val;
+
+        if (val < min || val > max) {
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+            // std::cin.ignore();
+        }
+        std::cin.ignore();
+    }
+
+    return val;
 }
 
 /*
@@ -54,4 +97,5 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    promptDouble("Please enter grade for assignment of " + 1, 0.000001, 1000.00);
 }
